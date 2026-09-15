e260ca2eb484b3c2af88419f22a30f1f0e7c0f17
Franco Fiuri <135768709+FrancoFiuri@users.noreply.github.com>
2026-09-15 12:35:53 -0300

    Arkanoid_Assets

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rigidBody2D;
+
+    public float speed = 300;
+
+
+    private Vector2 velocity;
+
+    private Vector2 startPosition;
+
+    public AudioSource audioSource;
+
+    public AudioClip playerSound, brickSound, wallSound, deadZoneSound;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        ResetBall();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeadZone"))
+        {
+            audioSource.clip = deadZoneSound;
+            audioSource.Play();
+            FindObjectOfType<GameManager>().LoseHealth();
+        }
+
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            audioSource.clip = playerSound;
+            audioSource.Play();
+        }
+
+        if (collision.gameObject.GetComponent<Brick>())
+        {
+            audioSource.clip = brickSound;
+            audioSource.Play();
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            audioSource.clip = wallSound;
+            audioSource.Play();
+        }
+
+    }
+
+    public void ResetBall()
+    {
+        transform.position = startPosition;
+        rigidBody2D.velocity = Vector2.zero;
+
+        velocity.x = Random.Range(-1f, 1f);
+
+        velocity.y = 1;
+
+        rigidBody2D.AddForce(velocity * speed);
+    }
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            FindAnyObjectByType<GameManager>().CheckLevelCompleted();
+
+            Destroy(gameObject);
+            
+        }
+
+    }
+
+
+}
